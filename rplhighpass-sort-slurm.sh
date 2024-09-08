5355835ac6b7aa6a1d902b8e29192c54ffbe5b0c
AWS ParallelCluster user <ec2-user@ip-10-0-1-108.ap-southeast-1.compute.internal>
2024-09-08 09:02:19 +0000

    modified:   rplhighpass-sort-slurm.sh

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -12,6 +12,11 @@
 #SBATCH -o rplhighpass*.%N.%j.out # STDOUT
 #SBATCH -e rplhighpass*.%N.%j.err # STDERR
 
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLLFP(saveLevel=1); \
@@ -25,3 +30,6 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
+
+conda deactivate 
+/data/src/PyHipp/envlist.py $envarg
