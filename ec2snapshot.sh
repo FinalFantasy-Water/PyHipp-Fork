ef60ea54c8d3aa06deeae2c5503d8c2a80c1be1a
FinalFantasy-Water <E1122340@u.nus.edu>
2024-09-19 14:16:11 +0800

    Create ec2snapshot.sh

---
+++ ec2snapshot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@54.255.163.225 "source ~/.bash_profile; pcluster update-compute-fleet --status STOP_REQUESTED -n MyCluster01; ~/update_snapshot.sh data 2 MyCluster01"
