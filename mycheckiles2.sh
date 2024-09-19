c2b1b8be684af699302e126a648f96d1178f1076
FinalFantasy-Water <E1122340@u.nus.edu>
2024-09-19 14:14:06 +0800

    Update mycheckiles2.sh

--- mycheckiles2.sh
+++ mycheckiles2.sh
@@ -6,14 +6,11 @@ find . -name "*.hkl" | grep -v -e spiketrain -e mountains | wc -l
 echo "Number of mda files"
 find mountains -name "firings.mda" | wc -l
 
-#===========================================================
 echo "Start Times"
-
-tail *slurm*.out
+head -n 1 *.out
 
 echo "End Times"
-
-#===========================================================
+tail -n 5 *.out 
 
 
 
